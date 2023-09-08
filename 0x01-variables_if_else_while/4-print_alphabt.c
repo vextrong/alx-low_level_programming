7e2e9752a3bd6b0adc1c8526427807051c47556d
vextrong <vextrong@gmail.com>
2023-09-08 02:48:10 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
