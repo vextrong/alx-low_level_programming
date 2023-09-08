8c55e0b0a9f66a4c2c0226a75a72dae1bf36310c
vextrong <vextrong@gmail.com>
2023-09-08 06:10:32 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - prints all possible combinations of single-digit numbers
- * Return: 0
- */
-int main(void)
-{
-	int n;
+#include <time.h>
 
-	for (n = '0'; n <= '9'; n++)
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(n);
-		if (n >= 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
 }
-
