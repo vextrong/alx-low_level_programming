6e8857dccf3d8795329c3821613f07e48218972b
vextrong <vextrong@gmail.com>
2023-09-15 04:33:50 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character to check
+ * Return: checks
+ */
+int _isupper(int c)
+{
+	
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (c);
+}
