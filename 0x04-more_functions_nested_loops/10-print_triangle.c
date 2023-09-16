4b69afacb3a8f3778a27b2250582c545d54826cc
vextrong <vextrong@gmail.com>
2023-09-16 00:47:06 -0700

    10-triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * print_triangle -  prints a triangle, followed by a new line
- * @size:  size of the triangle
- * Return: triangle
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
  */
 void print_triangle(int size)
 {
