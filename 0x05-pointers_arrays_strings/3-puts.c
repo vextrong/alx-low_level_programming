9290e26320e1f51c1c47993f607bf92976ef1df9
vextrong <vextrong@gmail.com>
2023-09-18 03:19:28 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  _puts -  prints a string, followed by a new line, to stdout
+ *  @str: string
+ *  Return: Success
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
